98f1319b1633a9aaaaf76780d23119642b73f675
francis omondi <francisomondi17@gmail.com>
2022-06-07 17:20:24 +0300

    displaying todo list using the .map method

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Form from "./components/Form";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
       <Todo />
-      <Form />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,17 +2,32 @@ import React from "react";
 import { useState } from "react";
 
 const Todo = () => {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState(" ");
+  const [todoList, setTodoList] = useState([]);
+
   const handleChange = event => {
     setTodo(event.target.value);
   };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    let tempList = todoList;
+    tempList.push(todo);
+    setTodoList(tempList);
+    console.log(todoList);
+    setTodo("");
+  };
+
   return (
     <div>
       <h1>Francis Todo App</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input value={todo} type="text" onChange={handleChange} />
-        <button type="submit">Submit</button>
+        <button type="submit">Add</button>
       </form>
+      {todoList.map(item => (
+        <h3>{item}</h3>
+      ))}
     </div>
   );
 };
