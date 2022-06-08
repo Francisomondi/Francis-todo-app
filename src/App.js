07fae8bfd3df917cc96e208fc0c977443b13224f
francis omondi <francisomondi17@gmail.com>
2022-06-08 12:25:53 +0300

    fresh intro to todo app..clean up and fresh start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import "./App.css";
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
